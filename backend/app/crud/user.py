162d72998845a896fdb59a2b6090a5cb40738987
sahanamar <texan.sahana@gmail.com>
2026-06-04 11:30:25 +0530

    Include backend in root repo

---
+++ backend/app/crud/user.py
@@ -0,0 +1,28 @@
+from sqlalchemy.orm import Session
+
+from app.models.user import User
+from app.schemas.user import UserCreate
+
+
+def create_user(db: Session, user: UserCreate):
+    db_user = User(
+        name=user.name,
+        email=user.email,
+        role=user.role
+    )
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+
+    return db_user
+
+def get_users(db: Session):
+    return db.query(User).all()
+
+def get_user(db: Session, user_id: int):
+    return (
+        db.query(User)
+        .filter(User.id == user_id)
+        .first()
+    )
---
+++ backend/app/models/user.py
@@ -0,0 +1,14 @@
+# from enum import Enum
+from sqlalchemy import Enum as SQLEnum
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+from app.enums.enums import UserRole
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(100), nullable=False)
+    email = Column(String(255), unique=True, nullable=False)
+    role = Column(SQLEnum(UserRole), nullable=False)
